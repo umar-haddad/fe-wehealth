e3d51cd3b561548f578fdd6fb38747dc553ea600
umar-haddad <sayyidumar11@gmail.com>
2023-11-15 22:25:05 +0700

    login

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from "react-router-dom"
-import Contoh from "./page/contoh/Contoh"
-import LandingPage from "./page/home/LandingPage"
+import { Route, Routes } from "react-router-dom";
+import Contoh from "./page/contoh/Contoh";
+import LandingPage from "./page/home/LandingPage";
+import Login from "./page/login/Login";
 
 const Router = () => {
-   return (
-      <Routes>
-         <Route path="/" element={<Contoh />} />
-         <Route path="/home" element={<LandingPage />} />
-      </Routes>
-   )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Contoh />} />
+      <Route path="/home" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+};
 
-export default Router
+export default Router;
