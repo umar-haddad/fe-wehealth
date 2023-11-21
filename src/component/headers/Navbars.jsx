ed60e37f33c8d5e067ee0ab2e35dddc96c8a19a6
prettyangela <angelala6600@gmail.com>
2023-11-21 21:16:44 +0700

    update react,

--- src/component/headers/Navbars.jsx
+++ src/component/headers/Navbars.jsx
@@ -17,17 +17,17 @@ function Navbars() {
               </a>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='./page/konsultasi.html'>
+              <a className='nav-link' href='/konsultasi'>
                 Konsultasi
               </a>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='./page/listarticle.html'>
+              <a className='nav-link' href='/artikel'>
                 Artikel
               </a>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='./page/klinik-center.html'>
+              <a className='nav-link' href='/klinik-center'>
                 Klinik Center
               </a>
             </li>
@@ -36,17 +36,14 @@ function Navbars() {
           <ul className='navbar-nav ms-auto mb-2 mb-lg-0 navbar-user'>
             <li className='nav-item nav-link-button'>
               <a
-                href='./page/login.html'
+                href='/login'
                 className='btn btn-outline-primary rounded-3 me-2'
               >
                 Login
               </a>
             </li>
             <li className='nav-item nav-link-button-2'>
-              <a
-                href='./page/register.html'
-                className='btn btn-primary rounded-3'
-              >
+              <a href='/register' className='btn btn-primary rounded-3'>
                 Sign Up
               </a>
             </li>
