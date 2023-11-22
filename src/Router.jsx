e1b5cd484a282c2a45e50669e4f4b11cda474dd9
rajastra <rajasaputeraxii.ipa5@gmail.com>
2023-11-22 07:49:35 +0700

    add layout for dashboard

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Contoh from "./page/contoh/Contoh"
 import LandingPage from "./page/home/LandingPage"
+import LayoutDasboard from "./layouts/dashboard/LayoutDasboard"
 
 const Router = () => {
    return (
       <Routes>
          <Route path="/" element={<Contoh />} />
          <Route path="/home" element={<LandingPage />} />
+         <Route path="/dashboard" element={<LayoutDasboard content={<h1>Hello world</h1>} />} />
       </Routes>
    )
 }
