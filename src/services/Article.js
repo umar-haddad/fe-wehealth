0277c86487ba0fdfc188c0e6ba0dcafcd103775a
rajastra <rajasaputeraxii.ipa5@gmail.com>
2023-11-22 23:39:40 +0700

    create article hooks api

---
+++ src/services/Article.js
@@ -0,0 +1,31 @@
+import { message } from 'antd';
+import axios from 'axios';
+
+const { VITE_BASE_URL } = import.meta.env;
+
+export const getArticle = async (url) => {
+  try {
+    const { data } = await axios.get(VITE_BASE_URL + url);
+    return data;
+  } catch (error) {
+    message.error(`Gagal mengambil data ( ${error.message} )`, 3);
+  }
+};
+
+export const deleteArticle = async (url) => {
+  try {
+    const { data } = await axios.delete(VITE_BASE_URL + url);
+    return data;
+  } catch (error) {
+    message.error(`Gagal menghapus data ( ${error.message} )`, 3);
+  }
+};
+
+export const getDetailArticle = async (url) => {
+  try {
+    const { data } = await axios.get(VITE_BASE_URL + url);
+    return data;
+  } catch (error) {
+    message.error(`Gagal mengambil data ( ${error.message} )`, 3);
+  }
+};
