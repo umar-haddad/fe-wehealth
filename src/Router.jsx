3d02b986315e053be3c862eca9397743ebbbedab
rajastra <rajasaputeraxii.ipa5@gmail.com>
2023-11-23 11:00:06 +0700

    update router

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,6 +5,8 @@ import Login from './page/login/Login';
 import LayoutDasboard from "./layouts/dashboard/LayoutDasboard"
 import Article from "./page/article/master/Article";
 import DetailArticle from "./page/article/detail/DetailArticle";
+import ArticleHomes from "./page/home/ArticleHomes";
+import ArticleDetail from "./component/article/ArticleDetail";
 
 const Router = () => {
   return (
@@ -15,6 +17,8 @@ const Router = () => {
       <Route path="/dashboard" element={<LayoutDasboard content={<h1>Hello world</h1>} />} />
       <Route path="/dashboard/article" element={<LayoutDasboard content={<Article />} />} />
       <Route path="/dashboard/article/:article_id" element={<LayoutDasboard content={<DetailArticle />} />} />
+      <Route path="/article" element={<ArticleHomes />} />
+      <Route path="/article/:article_id" element={<ArticleDetail />} />
     </Routes>
   );
 };
