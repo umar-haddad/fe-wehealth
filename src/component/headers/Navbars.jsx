017416ca1fab3da1d9f18b0797126d80bb454b50
prettyangela <angelala6600@gmail.com>
2023-11-28 20:21:15 +0700

    update navbars

--- src/component/headers/Navbars.jsx
+++ src/component/headers/Navbars.jsx
@@ -1,8 +1,17 @@
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
 function Navbars() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    if (Cookies.get('token')) {
+      setIsLogin(true);
+    }
+  }, []);
   return (
     <Navbar expand='lg' className='navbar navbar-expand-lg bg-white fixed-top'>
       <Container>
@@ -16,7 +25,7 @@ function Navbars() {
               </Link>
             </li>
             <li className='nav-item'>
-              <Link className='nav-link' to='/konsultasi'>
+              <Link className='nav-link' to='/dokter'>
                 Konsultasi
               </Link>
             </li>
@@ -33,19 +42,34 @@ function Navbars() {
           </ul>
 
           <ul className='navbar-nav ms-auto mb-2 mb-lg-0 navbar-user'>
-            <li className='nav-item nav-link-button'>
-              <Link
-                to='/login'
-                className='btn btn-outline-primary rounded-3 me-2'
-              >
-                Login
-              </Link>
-            </li>
-            <li className='nav-item nav-link-button-2'>
-              <Link to='/register' className='btn btn-primary rounded-3'>
-                Sign Up
-              </Link>
-            </li>
+            {isLogin ? (
+              <>
+                <li className='nav-item nav-link-button'>
+                  <Link
+                    to='/dashboard'
+                    className='btn btn-outline-primary rounded-3 me-2'
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className='nav-item nav-link-button'>
+                  <Link
+                    to='/login'
+                    className='btn btn-outline-primary rounded-3 me-2'
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li className='nav-item nav-link-button-2'>
+                  <Link to='/register' className='btn btn-primary rounded-3'>
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </Navbar.Collapse>
       </Container>
