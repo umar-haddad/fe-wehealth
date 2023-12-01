d1acd6bd69a2e0a02ce03dd62515e0b9c021cdd8
prettyangela <angelala6600@gmail.com>
2023-12-01 12:32:30 +0700

    update dokter detail

--- src/component/dokter/DokterDetail.jsx
+++ src/component/dokter/DokterDetail.jsx
@@ -7,14 +7,6 @@ function DokterDetail() {
   const { data: dataDetail, isLoading } = useDokterDetail(id, true);
 
   const data = dataDetail?.data?.dokter;
-  const date = new Date(data?.createdAt);
-  const options = {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-    timeZone: 'Asia/Jakarta',
-  };
-  const formattedDate = date.toLocaleDateString('id-ID', options);
 
   const navigate = useNavigate();
 
@@ -27,7 +19,7 @@ function DokterDetail() {
   };
 
   return (
-    <div className='container-xl margin-top-100'>
+    <div className='container-xl margin-top-100 mb-5'>
       <button
         className='btn btn-outline-secondary back-button mb-5'
         onClick={handleBack}
@@ -61,7 +53,11 @@ function DokterDetail() {
               <p>{data?.content}</p>
 
               {/* Button Chat Sekarang */}
-              <Button type='primary' className='mt-3' onClick={movePayment}>
+              <Button
+                type='primary'
+                className='mt-3 mb-5'
+                onClick={movePayment}
+              >
                 Chat Sekarang
               </Button>
             </div>
